cfac6df73be35e3c311e32300b52eda17bbbcf6d
Sakthivel J <sakthivel1023@gmail.com>
2023-06-06 15:15:08 +0530

    update name

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='video2images',
+    name='video2image',
     version='1.0.0',
     author='Sakthivel J',
     author_email='sakthivel1023@gmail.com',
     description='A Python script to extract frames from video files.',
-    url='https://github.com/sakthivelj/video2images',
+    url='https://github.com/sakthivelj/video2image',
     packages=find_packages(),
     install_requires=[
         'opencv-python',
     ],
     entry_points={
         'console_scripts': [
-            'video2images = video2images.__init__:main'
+            'video2image = video2image.__init__:main'
         ]
     }
 )
