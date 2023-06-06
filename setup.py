e448baec4b20c289cc4a893cdbd4f240b00ed396
sakthivel <37198254+sakthivelj@users.noreply.github.com>
2023-06-06 21:23:00 +0530

    Update version setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='video2image',
-    version='1.0.0',
+    version='1.0.1',
     author='Sakthivel J',
     author_email='sakthivel1023@gmail.com',
     description='A Python script to extract frames from video files.',
